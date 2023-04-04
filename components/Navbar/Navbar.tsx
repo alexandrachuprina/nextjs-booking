9d5306f83eefc6af7c7d4918d9b286c39bebba30
shplvk <114808997+shplvk@users.noreply.github.com>
2023-04-04 21:55:36 +0300

    feat: Navbar component (basics)

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,79 +1,52 @@
-import React, { useState } from "react";
-import Image from "next/image";
-
-import logo from "../../assets/images/logo.svg";
-
-import type { MenuProps } from "antd";
-import { Button, Menu, Row, Col } from "antd";
-
-import {
-  AppstoreOutlined,
-  ContainerOutlined,
-  DesktopOutlined,
-  MailOutlined,
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  PieChartOutlined,
-} from "@ant-design/icons";
-
-type MenuItem = Required<MenuProps>["items"][number];
-
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[],
-  type?: "group"
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-  } as MenuItem;
-}
-
-const items: MenuItem[] = [
-  getItem("Home", "1", <PieChartOutlined />),
-  getItem("the hut", "2", <DesktopOutlined />),
-  getItem("The area", "3", <ContainerOutlined />),
-  getItem("Booking", "4", <ContainerOutlined />),
-  getItem("About us", "5", <ContainerOutlined />),
-];
+import React, { useState, useEffect } from "react";
+import { Row, Col, Button, Drawer, Divider } from "antd";
+import styles from "../../styles/components/Navbar.module.scss";
 
 function Navbar() {
-  const [collapsed, setCollapsed] = useState(false);
+  const [phone, setPhone] = useState<any>();
+  const [open, setOpen] = useState<any>(false);
 
-  const toggleCollapsed = () => {
-    setCollapsed(!collapsed);
+  useEffect(() => {
+    const width = window.innerWidth;
+    if (width < 768) {
+      setPhone(true);
+    } else {
+      setPhone(false);
+    }
+  }, []);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
   };
 
   return (
-    <div className="component">
-      <Row justify={"center"}>
-        <Col span={24}>
-          <Image src={logo} alt={"logo"} height={50} width={50} />
-          <div style={{ width: 256 }}>
-            <Button
-              type="primary"
-              onClick={toggleCollapsed}
-              style={{ marginBottom: 16 }}
-            >
-              {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            </Button>
-            <Menu
-              defaultSelectedKeys={["1"]}
-              defaultOpenKeys={["sub1"]}
-              mode="inline"
-              theme="dark"
-              inlineCollapsed={collapsed}
-              items={items}
-            />
-          </div>
-        </Col>
-      </Row>
-    </div>
+    <>
+      {phone ? (
+        <div className={styles.component}>
+          <Row justify={"center"}>
+            <Col span={22}>
+              <button onClick={showDrawer}>Open</button>
+
+              <Drawer
+                title="Menu"
+                placement="right"
+                onClose={onClose}
+                open={open}
+                width={150}
+              >
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+              </Drawer>
+            </Col>
+          </Row>
+        </div>
+      ) : <p>no phone</p>}
+    </>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,14 @@ import styles from '@/styles/Home.module.css'
 import Facilities from '@/components/Facilities'
 import Contacts from '@/components/Contacts'
 import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
+    <Navbar/>
     <Header/>
     <Main/>
     <Map/>
