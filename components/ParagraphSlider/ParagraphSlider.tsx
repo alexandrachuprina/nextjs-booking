9ef583c24575a07ae34c63f3ddcf53f93792b7a8
shplvk <114808997+shplvk@users.noreply.github.com>
2023-04-05 18:21:40 +0300

    feat: ParagraphSlider component

--- components/ParagraphSlider/ParagraphSlider.tsx
+++ components/ParagraphSlider/ParagraphSlider.tsx
@@ -1,18 +1,53 @@
 import React from "react";
+import Image from "next/image";
+import { Row, Col } from "antd";
+import styles from "../../styles/components/ParagraphSlider.module.scss";
 
 interface Props {
   header: string;
   description: string;
   link: string;
+  image: string;
+  reverse: boolean;
 }
 
 function ParagraphSlider(props: Props) {
   return (
-    <>
-      <h2>{props.header}</h2>
-      <p>{props.description}</p>
-      <p>{props.link}</p>
-    </>
+    <Row justify={"center"}>
+      <Col span={22}>
+        {props.reverse ? (
+          <>
+            <div className={styles.component}>
+              <div className={styles.text}>
+                <h2>{props.header}</h2>
+                <p>{props.link}</p>
+                <p>{props.description}</p>
+              </div>
+              <div className={styles.photos}>
+                <div className={styles.photo}>
+                  <Image src={props.image} alt={props.header} fill={true} />
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={styles.component}>
+              <div className={styles.photos}>
+                <div className={styles.photo}>
+                  <Image src={props.image} alt={props.header} fill={true} />
+                </div>
+              </div>
+              <div className={styles.text}>
+                <h2>{props.header}</h2>
+                <p>{props.link}</p>
+                <p>{props.description}</p>
+              </div>
+            </div>
+          </>
+        )}
+      </Col>
+    </Row>
   );
 }
 
--- pages/hut.tsx
+++ pages/hut.tsx
@@ -1,12 +1,60 @@
 import React from "react";
 import HeaderImage from "@/components/HeaderImage/HeaderImage";
+import ParagraphSlider from "@/components/ParagraphSlider/ParagraphSlider";
 
 import hut01 from "../assets/images/hut01.png";
+import hut02 from "../assets/images/hut02.png";
+import hut03 from "../assets/images/hut03.png";
+import hut04 from "../assets/images/hut04.png";
+
+// To create slider component
+// and for text
 
 function hut() {
   return (
-    // @ts-ignore
-    <HeaderImage header="The Hut" image={hut01} />
+    <>
+      <HeaderImage
+        header="The Hut"
+        // @ts-ignore
+        image={hut01}
+      />
+      <ParagraphSlider
+        header="The Outside"
+        description="Løvtag’s three exclusive and comfortable cabins, Et, 
+        Ro and Ly, are designed by architect Sigurd Larsen. They all have an 
+        open space with a double bed, a double sofa bed, kitchen, separate 
+        toilet and an outdoor shower. On the roof, surrounded by treetops, t
+        here is a terrace, which is about nine meters above ground. "
+        link="Hot tube"
+        // @ts-ignore
+        image={hut02}
+        reverse={false}
+      />
+      <ParagraphSlider
+        header="The Inside"
+        description="Løvtag’s three exclusive and comfortable cabins, Et, 
+        Ro and Ly, are designed by architect Sigurd Larsen. They all have an 
+        open space with a double bed, a double sofa bed, kitchen, separate 
+        toilet and an outdoor shower. On the roof, surrounded by treetops, t
+        here is a terrace, which is about nine meters above ground. "
+        link="Hot tube"
+        // @ts-ignore
+        image={hut03}
+        reverse={1 < 0 ? true : false}
+      />
+      <ParagraphSlider
+        header="Practicalities"
+        description="Løvtag’s three exclusive and comfortable cabins, Et, 
+        Ro and Ly, are designed by architect Sigurd Larsen. They all have an 
+        open space with a double bed, a double sofa bed, kitchen, separate 
+        toilet and an outdoor shower. On the roof, surrounded by treetops, t
+        here is a terrace, which is about nine meters above ground. "
+        link="Hot tube"
+        // @ts-ignore
+        image={hut04}
+        reverse={false}
+      />
+    </>
   );
 }
 
