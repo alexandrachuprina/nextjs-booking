b64da98a232e3cd2eb112b4200d09f10d6ee6128
shplvk <114808997+shplvk@users.noreply.github.com>
2023-04-06 13:28:12 +0300

    feat: Slider component (no props)

--- pages/hut.tsx
+++ pages/hut.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import HeaderImage from "@/components/HeaderImage/HeaderImage";
 import ParagraphSlider from "@/components/ParagraphSlider/ParagraphSlider";
 
+import Slider from "@/components/ParagraphSlider/Slider";
+
 import hut01 from "../assets/images/hut01.png";
 import hut02 from "../assets/images/hut02.png";
 import hut03 from "../assets/images/hut03.png";
@@ -18,6 +20,7 @@ function hut() {
         // @ts-ignore
         image={hut01}
       />
+      <Slider/>
       <ParagraphSlider
         header="The Outside"
         description="Løvtag’s three exclusive and comfortable cabins, Et, 
