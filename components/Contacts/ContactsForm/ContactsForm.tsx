9b38010cf7f03ec3c2fc6de0342b652d9cd53070
shplvk <114808997+shplvk@users.noreply.github.com>
2023-04-26 12:24:53 +0300

    feat: ContactsForm, add button

--- components/Contacts/ContactsForm/ContactsForm.tsx
+++ components/Contacts/ContactsForm/ContactsForm.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Row, Col } from "antd";
 import styles from "../../../styles/components/Contacts/CotactsForm.module.scss";
-import input from '../../../styles/inputs/ContactsFromInput.module.scss';
-import ContactsLogo from "../ContactsLogo";
+import button from "../../../styles/buttons/BookingButton.module.scss";
+import input from "../../../styles/inputs/ContactsFromInput.module.scss";
 
 function ContactsForm() {
   return (
@@ -10,12 +10,34 @@ function ContactsForm() {
       <Col xs={24}>
         <div className={styles.component}>
           <div className={styles.fullname}>
-            <input className={input.component} type="text" placeholder="First Name" />
-            <input className={input.component} type="text" placeholder="Last Name" />
+            <input
+              className={input.component}
+              type="text"
+              placeholder="First Name"
+            />
+            <input
+              className={input.component}
+              type="text"
+              placeholder="Last Name"
+            />
           </div>
           <input className={input.component} type="email" placeholder="Email" />
-          <input className={input.component} type="text" placeholder="Subject" />
-          <textarea className={styles.textarea} name="" id="01" placeholder="Message" />
+          <input
+            className={input.component}
+            type="text"
+            placeholder="Subject"
+          />
+          <textarea
+            className={styles.textarea}
+            name=""
+            id="01"
+            placeholder="Message"
+          />
+        </div>
+        <div className={styles.submit}>
+          <button className={button.component}>
+            <p>Submit</p>
+          </button>
         </div>
       </Col>
     </Row>
