ec86f7f8fa696c4d2e030d80d19d8204580bda52
shplvk <114808997+shplvk@users.noreply.github.com>
2023-04-28 15:40:47 +0300

    'fix: GalleryRow component'

--- components/Gallery/GalleryRow/GalleryRow.tsx
+++ components/Gallery/GalleryRow/GalleryRow.tsx
@@ -11,11 +11,7 @@ interface Props {
 function GalleryRow(props: Props) {
   return (
     <Row justify={"center"}>
-<<<<<<< HEAD
-      <Col xs={22} sm={20} lg={22} xl={23} xxl={23}>
-=======
       <Col xs={22} sm={20} lg={22} xl={23} flex={'1600px'}>
->>>>>>> develop
         <div className={styles.component}>
           {props.links.map((elem: any, i: number) => (
             <div className={styles.image} key={i}>
